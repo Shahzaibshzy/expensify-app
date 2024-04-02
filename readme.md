6b5e260ac562452b2138a890f32dcb716129e673
Shahzaibshzy <112001025+Shahzaibshzy@users.noreply.github.com>
2024-04-02 05:33:27 +0500

    setup prod webpack build

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Git commands 
+
+git init - create new repo
+
+git status - let you view the changes in project
+
+git add - add files to staging area
+
+git commit -m "message"  - create new commit  with message from staging area
+
+git log - view recent commits
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,46 @@
-const path = require('path');
-module.exports = {
-
-    mode: 'development',
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  return {
+    mode: "development",
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-      },
-      module:{
-        rules:[{
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
+    ],
+    module: {
+      rules: [
+        {
           loader: `babel-loader`,
           test: /\.js$/,
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
-      {
-        test : /\.s?css$/,
-        use  : [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }]
-      },
-      devtool: "cheap-module-source-map",
-      devServer: {
-        static: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-        compress: true,
-        port: 9000
-      }
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: {
+              sourceMap : true
+            } },
+            { loader: "sass-loader", options: {
+              sourceMap: true
+            } },
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      static: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      compress: true,
+      port: 9000,
+    },
+  };
 };
